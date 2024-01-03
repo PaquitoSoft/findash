1946b722bd821a408fa0cc29ec75cfa87e4a135c
PaquitoSoft <paquitosoftware@gmail.com>
2024-01-03 14:29:40 +0100

    refactor: return transactions types ordered by code

--- server/trpc/routes/get-transaction-types.ts
+++ server/trpc/routes/get-transaction-types.ts
@@ -1,8 +1,9 @@
+import sortBy from 'lodash/sortBy';
 import { getTransactionTypes } from '~/server/dal/transaction-type';
 import { publicProcedure } from '~/server/trpc/trpc';
 
 export const getTransactionTypesProcedure = publicProcedure
     .query(async () => {
       const types = await getTransactionTypes();
-      return { types };
+      return { types: sortBy(types, (type) => type.code) };
     });
